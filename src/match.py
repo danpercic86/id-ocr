c8d3c2cfbfb78b8068de4cb5116ce5ab7c1915a3
Dan Percic <danpercic86@gmail.com>
2022-02-08 22:43:59 +0200

    new algorithm

--- src/match.py
+++ src/match.py
@@ -2,33 +2,33 @@ import numpy as np
 from cv2 import cv2
 from numpy.typing import NDArray
 
-orb = cv2.ORB_create(5000)
+orb = cv2.ORB_create(1000)
 
 percentage = 25
 
 
 def match(img: NDArray, template: NDArray):
-    key_points1, descriptors1 = orb.detectAndCompute(template, None)
-    # img_key_points = cv2.drawKeypoints(template, key_points1, None)
-    key_points2, descriptors2 = orb.detectAndCompute(img, None)
+    template_key_points, template_descriptors = orb.detectAndCompute(cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), None)
+    # img_key_points = cv2.drawKeypoints(template, template_key_points, None)
+    img_key_points, img_descriptors = orb.detectAndCompute(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), None)
 
     # create BFMatcher object
     bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 
     # match descriptors
-    matches = list(bf.match(descriptors2, descriptors1))
+    matches = list(bf.match(img_descriptors, template_descriptors))
 
     # sort them in order of their distance
     matches.sort(key=lambda x: x.distance)
 
     # draw first "percentage" matches. percentage is set to 25
     good = matches[:int(len(matches) * (percentage / 100))]
-    img_match = cv2.drawMatches(img, key_points2, template, key_points1, good[:100], None, flags=2)
+    img_match = cv2.drawMatches(img, img_key_points, template, template_key_points, good[:100], None, flags=2)
 
     # we take all the points in our good matches list and we use them
     # to align the corners of our input image
-    src_points = np.float32([key_points2[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    dst_points = np.float32([key_points1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+    src_points = np.float32([img_key_points[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    dst_points = np.float32([template_key_points[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
     M, _ = cv2.findHomography(src_points, dst_points, cv2.RANSAC, 5.0)
 
